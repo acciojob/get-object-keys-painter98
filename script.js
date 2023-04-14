4f6f96d5fadef60baa4618b7c2fecd628fbf95e2
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-14 18:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ let student = {
 	}
 
 let keys = student.getKeys();
+
+ const myObj = { name: "John", age: 30, city: "New York" }; 
+		Object.prototype.getKeys = ()=>{
+		return Object.keys(myObj);
+	}
